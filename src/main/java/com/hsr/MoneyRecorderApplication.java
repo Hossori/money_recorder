1dd32f7e82e84e3e0ca865dd6a76cd0219b53b96
Hossori <law.f1.vegas@gmail.com>
2023-01-30 14:32:00 +0900

    initial commit.

---
+++ src/main/java/com/hsr/MoneyRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.hsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyRecorderApplication.class, args);
+	}
+
+}
